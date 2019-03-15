7ca56bd278deff1692d1aedb896520e3ec92e7b3
Carsten Bleek <bleek@cross-solution.de>
2019-03-15 13:23:10 +0000

    adds writable dir

--- deploy.php
+++ deploy.php
@@ -16,7 +16,7 @@ add('shared_dirs', [
 ]);
 
 // Writable dirs by web server 
-add('writable_dirs', []);
+add('writable_dirs', ['/var/www/production/current/shared/']);
 
 set('default_stage', 'prod');
 
