cca6c20b4e5487ca6919450ff9c9ab190a229323
Carsten Bleek <bleek@cross-solution.de>
2021-04-30 18:22:18 +0200

    chore: update dependencies
    https://gitlab.cross-solution.de/Personalwerk/yawik/form2mail/-/issues/6

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -1,26 +1,50 @@
 module.exports = function(grunt) {
+    var targetDir = grunt.config.get('targetDir');
+    var nodeModulesPath = grunt.config.get('nodeModulesPath');
 
-  var targetDir = grunt.config.get('targetDir');
-  var moduleDir = targetDir + "/modules/Form2Mail";
-  
-  grunt.config.merge({
-    less: {
-      form2mail: {
-        options: {
-          compress: true,
-          optimization: 2
-        },
-        files: [
-            {
-              dest: targetDir + "/modules/Form2Mail/layout.css",
-              src: moduleDir + "/less/layout.less"
+    grunt.config.merge({
+        less: {
+            form2mail: {
+                options: {
+                    compress: false,
+                    modifyVars: {
+                        "fa-font-path": "/dist/fonts",
+                        "flag-icon-css-path": "/dist/flags"
+                    }
+                },
+                files: [
+                    {
+                        src: [
+                            "./node_modules/select2/dist/css/select2.min.css",
+                            "./node_modules/pnotify/dist/pnotify.css",
+                            "./node_modules/pnotify/dist/pnotify.buttons.css",
+                            "./node_modules/bootsrap3-dialog/dist/css/bootstrap-dialog.css",
+                            targetDir+"/modules/Aviation/less/layout.less"
+                        ],
+                        dest: targetDir+"/modules/Aviation/dist/layout.css"
+                    }
+                ]
+            },
+            jobs: {
+                files: [
+                    {
+                        src: "./view/jobs/templates/less/job.less",
+                        dest: "./view/jobs/templates/job.css"
+                    }
+                ]
             }
-          ]
-        
-      }
-    }
-  });
+        },
+        cssmin: {
+            aviation: {
+                files: [
+                    {
+                        dest: targetDir+'/modules/Aviation/dist/layout.min.css',
+                        src: targetDir+'/modules/Aviation/dist/layout.css'
+                    }
+                ]
+            }
+        }
+    });
 
-  grunt.registerTask('yawik:form2mail', ['less:form2mail']);
+    grunt.registerTask('yawik:form2mail',['copy','less','less:job','concat','uglify','cssmin']);
 };
-
